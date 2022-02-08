4d1bb88cd3a5048a53b2b96d1affeca736823a14
Gabriela Gheorghe <gabrielagheorghe1998@gmail.com>
2022-02-08 13:34:01 +0200

    update indendation

--- blog.js
+++ blog.js
@@ -1,42 +1,40 @@
 //Create a new array with 5 objects. Every object should have an id, a title, description, date when was added, and a image.
 //You can populate these fields with the data you have on blogposts.  In the end console log the list with the 5 objects.
-const blog = [
-    {
-        id: 1,
-        title: 'Sagrada Familia',
-        description: 'The most famous sight and tourist attraction is certainly the historic church Sagrada Familia',
-        date: '2021.10.01',
-        image: 'https://www.proidea.ro/aplicatii-produse-5/mapei-sagrada-familia-barcelona-14513/a_49_d_15_1518696636579_mapei_barcelona_sagrada_familia_1.jpg'
-    },
-    {
-        id: 2,
-        title: 'Parc Guell',
-        description: 'Park Güell is the famous city park with the mosaics and the salamander in the district Gràcia of Barcelona and definitely one of the most important attractions in Barcelona in terms of visitor numbers and architecture',
-        date: '2021.05.03',
-        image: 'https://images.unsplash.com/photo-1544918877-460635b6d13e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80'
-    },
-    {
-        id: 3,
-        title: 'Alcazar de Sevilla',
-        description: 'The Real Alcázar de Sevilla is the Royal Palace of Seville. The name Alcazar means castle in Spanish and is derived from the Arabic word al-qasr (fortress or palace)',
-        date: '2021.12.06',
-        image: 'https://images.unsplash.com/photo-1611420278664-07c3e3ab4aea?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1169&q=80'
-    },
-    {
-        id: 4,
-        title: 'Metropol Parasol Seville',
-        description: 'Standing in the Plaza de la Encarnación is Metropol Parasol, also known as the mushrooms of Seville',
-        date: '2021.02.09',
-        image: 'https://c1.wallpaperflare.com/preview/87/936/58/metropol-parasol-parasol-metropol-seville.jpg'
-    },
-    {
-        id: 5,
-        title: 'Setenil de las Bodegas',
-        description: 'The municipal area is located in the north east of the province, in the continuation of the Ronda Valley, and its landscape comprises gently undulating fields of cereals and olive trees, across which the River Guadalporcun flows',
-        date: '2021.06.12',
-        image: 'https://c1.wallpaperflare.com/preview/385/377/71/white-people-andalusia-white-houses-setenil.jpg '
-    }
-]
+const blog = [{
+    id: 1,
+    title: 'Sagrada Familia',
+    description: 'The most famous sight and tourist attraction is certainly the historic church Sagrada Familia',
+    date: '2021.10.01',
+    image: 'https://www.proidea.ro/aplicatii-produse-5/mapei-sagrada-familia-barcelona-14513/a_49_d_15_1518696636579_mapei_barcelona_sagrada_familia_1.jpg'
+},
+{
+    id: 2,
+    title: 'Parc Guell',
+    description: 'Park Güell is the famous city park with the mosaics and the salamander in the district Gràcia of Barcelona and definitely one of the most important attractions in Barcelona in terms of visitor numbers and architecture',
+    date: '2021.05.03',
+    image: 'https://images.unsplash.com/photo-1544918877-460635b6d13e?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1176&q=80'
+},
+{
+    id: 3,
+    title: 'Alcazar de Sevilla',
+    description: 'The Real Alcázar de Sevilla is the Royal Palace of Seville. The name Alcazar means castle in Spanish and is derived from the Arabic word al-qasr (fortress or palace)',
+    date: '2021.12.06',
+    image: 'https://images.unsplash.com/photo-1611420278664-07c3e3ab4aea?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1169&q=80'
+},
+{
+    id: 4,
+    title: 'Metropol Parasol Seville',
+    description: 'Standing in the Plaza de la Encarnación is Metropol Parasol, also known as the mushrooms of Seville',
+    date: '2021.02.09',
+    image: 'https://c1.wallpaperflare.com/preview/87/936/58/metropol-parasol-parasol-metropol-seville.jpg'
+},
+{
+    id: 5,
+    title: 'Setenil de las Bodegas',
+    description: 'The municipal area is located in the north east of the province, in the continuation of the Ronda Valley, and its landscape comprises gently undulating fields of cereals and olive trees, across which the River Guadalporcun flows',
+    date: '2021.06.12',
+    image: 'https://c1.wallpaperflare.com/preview/385/377/71/white-people-andalusia-white-houses-setenil.jpg '
+}]
 console.log(blog);
 
 //Create a method addPost(){} and here try to add 2 more post object to original array. console.log the array after this change. Should have 7 items now. 
@@ -92,7 +90,6 @@ const selectOddPost = (blog) => {
     let odd = [];
     let even = [];
     blog.forEach(element => {
-
         if (element.id % 2 === 0)
             even.push(element)
         else
